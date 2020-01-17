b510a7d12143b78c1c84638ab665a43e7f2473f2
姜伟 <adsl305562676@163.com>
2020-01-17 16:15:57 +0800

    PINCache: 3.0.1 Beta 5 update:3.0.1 Beta 8 SQLite: 3.19.03.00   update:3.30.1

--- Benchmark/Vendor/YYThreadSafeDictionary/YYThreadSafeDictionary.m
+++ Benchmark/Vendor/YYThreadSafeDictionary/YYThreadSafeDictionary.m
@@ -136,27 +136,27 @@ OSSpinLockUnlock(&_lock);
     LOCK(id o = [_dic objectForKeyedSubscript:key]); return o;
 }
 
-- (void)enumerateKeysAndObjectsUsingBlock:(void (^)(id key, id obj, BOOL *stop))block {
+- (void)enumerateKeysAndObjectsUsingBlock:(void (^)(id key, id obj, BOOL *stop)) __attribute__((noescape))block {
     LOCK([_dic enumerateKeysAndObjectsUsingBlock:block]);
 }
 
-- (void)enumerateKeysAndObjectsWithOptions:(NSEnumerationOptions)opts usingBlock:(void (^)(id key, id obj, BOOL *stop))block {
+- (void)enumerateKeysAndObjectsWithOptions:(NSEnumerationOptions)opts usingBlock:(void (^)(id key, id obj, BOOL *stop))__attribute__((noescape))block {
     LOCK([_dic enumerateKeysAndObjectsWithOptions:opts usingBlock:block]);
 }
 
-- (NSArray *)keysSortedByValueUsingComparator:(NSComparator)cmptr {
+- (NSArray *)keysSortedByValueUsingComparator:(NSComparator)__attribute__((noescape))cmptr {
     LOCK(NSArray * a = [_dic keysSortedByValueUsingComparator:cmptr]); return a;
 }
 
-- (NSArray *)keysSortedByValueWithOptions:(NSSortOptions)opts usingComparator:(NSComparator)cmptr {
+- (NSArray *)keysSortedByValueWithOptions:(NSSortOptions)opts usingComparator:(NSComparator)__attribute__((noescape))cmptr {
     LOCK(NSArray * a = [_dic keysSortedByValueWithOptions:opts usingComparator:cmptr]); return a;
 }
 
-- (NSSet *)keysOfEntriesPassingTest:(BOOL (^)(id key, id obj, BOOL *stop))predicate {
+- (NSSet *)keysOfEntriesPassingTest:(BOOL (^)(id key, id obj, BOOL *stop))__attribute__((noescape))predicate {
     LOCK(NSSet * a = [_dic keysOfEntriesPassingTest:predicate]); return a;
 }
 
-- (NSSet *)keysOfEntriesWithOptions:(NSEnumerationOptions)opts passingTest:(BOOL (^)(id key, id obj, BOOL *stop))predicate {
+- (NSSet *)keysOfEntriesWithOptions:(NSEnumerationOptions)opts passingTest:(BOOL (^)(id key, id obj, BOOL *stop))__attribute__((noescape))predicate {
     LOCK(NSSet * a = [_dic keysOfEntriesWithOptions:opts passingTest:predicate]); return a;
 }
 
